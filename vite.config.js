f9b73a4c13c7b4462aa1c1fe7c7cd79b4b1f3ec8
SAI30570SREE@Rachapudi <144793915+SAI30570SREERachapudi@users.noreply.github.com>
2025-11-16 23:24:15 +0530

    Update base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/s111-skill3-30570/'
+  base: '/s111-skill3-30570-git-actions-frontend/'
 });
